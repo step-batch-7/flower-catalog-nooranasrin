83529f650ea154e849ddb1c2cfafc7753c33c138
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-01-26 11:21:09 +0530

    Added 404 error page

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 const fs = require('fs');
 const Response = require('./lib/response');
 const CONTENT_TYPES = require('./lib/mimeTypes');
+const { loadTemplate } = require('./lib/loadTemplate');
 
 const STATIC_FOLDER = `${__dirname}/public`;
 
-const serveStaticFile = req => {
-  const path = `${STATIC_FOLDER}${req.url}`;
-  const stat = fs.existsSync(path) && fs.statSync(path);
-  if (!stat || !stat.isFile()) return new Response();
+const provideResponse = function(path, statusCode, content) {
   const [, extension] = path.match(/.*\.(.*)$/) || [];
   const contentType = CONTENT_TYPES[extension];
+  const response = new Response();
+  response.setHeader('Content-Type', contentType);
+  response.setHeader('Content-Length', content.length);
+  response.statusCode = statusCode;
+  response.body = content;
+  return response;
+};
+
+const serveFile = req => {
+  const path = `${STATIC_FOLDER}${req.url}`;
+  const stat = fs.existsSync(path) && fs.statSync(path);
+  if (!stat || !stat.isFile()) {
+    const content = loadTemplate('error.html', { URL: req.url });
+    return provideResponse('error.html', 404, content);
+  }
   const content = fs.readFileSync(path);
-  const res = new Response();
-  res.setHeader('Content-Type', contentType);
-  res.setHeader('Content-Length', content.length);
-  res.statusCode = 200;
-  res.body = content;
-  return res;
+  return provideResponse(path, 200, content);
 };
 
 const findHandler = req => {
   if (req.method === 'GET' && req.url === '/') {
     req.url = '/index.html';
-    return serveStaticFile;
+    return serveFile;
   }
-  if (req.method === 'GET') return serveStaticFile;
+  if (req.method === 'GET') return serveFile;
   return () => new Response();
 };
 
