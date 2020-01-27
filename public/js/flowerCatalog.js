69f866e481833f4085c743e401affdc7e0f1b390
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-01-27 07:34:56 +0530

    updated the hide function
    previously the function was changing the inner text of the img tag
    now using style.visibility property to do that

--- public/js/flowerCatalog.js
+++ public/js/flowerCatalog.js
@@ -1,5 +1,5 @@
 const hide = function() {
-  const flowerJar = event.target.src;
-  event.target.src = '';
-  setTimeout(event => (event.target.src = flowerJar), 1000, event);
+  const target = event.target;
+  target.style.visibility = 'hidden';
+  setTimeout(() => (target.style.visibility = 'visible'), 1000);
 };
