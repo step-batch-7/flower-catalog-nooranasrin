f5ab59c5a8ebdc9392f59712361122216a01f3de
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-01-29 14:55:35 +0530

    Added a function to send the response

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const http = require('http');
 const { methods } = require('./lib/handlers');
 
-const processRequest = function(request, comment) {
+const processRequest = function(request, response) {
   const handlers = methods[request.method] || methods.NOT_ALLOWED;
   const handler = handlers[request.url] || handlers.default;
-  return handler(request, comment);
+  return handler(request, response);
 };
 
-const handleRequest = function(request, response) {
-  let comment = '';
-  request.on('data', chunk => (comment += chunk));
-  request.on('end', () => {
-    const { contentType, statusCode, body } = processRequest(request, comment);
-    response.setHeader('Content-Type', contentType);
-    response.writeHead(statusCode);
-    response.end(body);
-  });
-};
+// const handleRequest = function(request, response) {
+//   let comment = '';
+//   request.on('data', chunk => (comment += chunk));
+//   request.on('end', () => {
+//     const { contentType, statusCode, body } = processRequest(request, comment);
+//     response.setHeader('Content-Type', contentType);
+//     response.writeHead(statusCode);
+//     response.end(body);
+//   });
+// };
 
 const main = function(port = 4000) {
-  const server = http.createServer(handleRequest);
+  const server = http.createServer(processRequest);
   server.on('error', err => console.error('server error', err));
   server.listen(port, () =>
     console.warn('started listening', server.address())
