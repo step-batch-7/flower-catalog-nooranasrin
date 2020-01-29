ea76b4c555cfc745be5883111bb30af3df08df4f
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-01-29 16:29:58 +0530

    Extracted App class that is capable of calling the appropriate handlers

--- server.js
+++ server.js
@@ -1,24 +1,8 @@
 const http = require('http');
-const { methods } = require('./lib/handlers');
-
-const matchingHandler = function(request, handler) {
-  return request.url.match(handler.path);
-};
-
-const processRequest = function(request, response) {
-  const handlers = methods[request.method];
-  const matchingHandlers = handlers.filter(matchingHandler.bind(null, request));
-
-  const next = function() {
-    const router = matchingHandlers.shift();
-    router.handler(request, response, next);
-  };
-
-  next();
-};
+const { app } = require('./lib/handlers');
 
 const main = function(port = 4000) {
-  const server = http.createServer(processRequest);
+  const server = http.createServer(app.processRequest.bind(app));
   server.on('error', err => console.error('server error', err));
   server.listen(port, () =>
     console.warn('started listening', server.address())
