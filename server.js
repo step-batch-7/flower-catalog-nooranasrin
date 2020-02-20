4ed8e9e8891d4270261acd4eead9ccb1c50f3d76
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-02-20 12:51:03 +0530

    added port

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const http = require('http');
 const { stdout, stderr } = process;
 const { app } = require('./lib/handlers');
-const [, , port] = process.argv;
-const DEFAULT_PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
-const main = function(port = DEFAULT_PORT) {
+const main = function(port) {
   const server = http.createServer((req, res) => app.processRequest(req, res));
   server.on('error', err => stderr.write('server error', err));
   server.listen(port, () =>
