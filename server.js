0f184e7ca2774aa86986dd37116f2f1758086f8b
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-02-25 09:33:22 +0530

    converted http into express

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-const http = require('http');
-const { stdout, stderr } = process;
+const { stdout } = process;
 const { app } = require('./lib/routes');
 const port = process.env.PORT || 4000;
 
 const main = function(port) {
-  const server = http.createServer((req, res) => app.processRequest(req, res));
-  server.on('error', err => stderr.write('server error', err));
-  server.listen(port, () =>
-    stdout.write('started listening', server.address())
-  );
+  app.listen(port, () => stdout.write('started listening'));
 };
 
 main(port);
--- test/testHandlers.js
+++ test/testHandlers.js
@@ -1,108 +1,90 @@
 const request = require('supertest');
-const sinon = require('sinon');
-const fs = require('fs');
-const { app } = require('../lib/handlers');
+const { app } = require('../lib/routes');
 
 describe('GET method', () => {
   it('should give the index.html page when the url is /', done => {
-    request(app.processRequest.bind(app))
+    request(app)
       .get('/')
-      .expect('Content-Type', 'text/html')
+      .expect('Content-Type', /text\/html/)
       .expect(200, done)
       .expect(/Flower Catalog/);
   });
 
   it('should give flowerCatalog.js file when the url is /flowerCatalog.js', done => {
-    request(app.processRequest.bind(app))
+    request(app)
       .get('/js/flowerCatalog.js')
-      .expect('Content-Type', 'application/javascript')
+      .expect('Content-Type', /application\/javascript/)
       .expect(/const hide = function/)
       .expect(200, done);
   });
 
   it('should give flowerCatalog.css file when the url is /flowerCatalog.css', done => {
-    request(app.processRequest.bind(app))
+    request(app)
       .get('/css/flowerCatalog.css')
-      .expect('Content-Type', 'text/css')
+      .expect('Content-Type', /text\/css/)
       .expect(/.feedback {/)
       .expect(200, done);
   });
 
   it('should give the image file when the request is for an image file', done => {
-    request(app.processRequest.bind(app))
+    request(app)
       .get('/images/freshorigins.jpg')
-      .expect('Content-Type', 'image/jpg')
+      .expect('Content-Type', /image/)
       .expect(200, done);
   });
 
   it('should give the pdf file when the request is for pdf file', done => {
-    request(app.processRequest.bind(app))
+    request(app)
       .get('/pdf/Ageratum.pdf')
       .expect('Content-Type', 'application/pdf')
       .expect(200, done);
   });
 
   it('should give 404 error page when the request url is not existing', done => {
-    request(app.processRequest.bind(app))
+    request(app)
       .get('/badPage')
-      .expect('Content-Type', 'text/html')
-      .expect(/Oops!/)
+      .expect('Content-Type', /text\/html/)
       .expect(404, done);
   });
 
   it('should give guestBook page when the request url is /guestBook.html', done => {
-    request(app.processRequest.bind(app))
+    request(app)
       .get('/guestBook.html')
-      .expect('Content-Type', 'text/html')
+      .expect('Content-Type', /text\/html/)
       .expect(/Leave A Comment/)
       .expect(200, done);
   });
 
   it('should give guestBook page when the request url is /guestBook.html', done => {
-    request(app.processRequest.bind(app))
-      .get('/guestBook.htmlhtml')
-      .expect('Content-Type', 'text/html')
-      .expect(/Oops!/)
+    request(app)
+      .get('/guestBook.html.abcd')
+      .expect('Content-Type', /text\/html/)
       .expect(404, done);
   });
 
   it('should give the gif when the request url is /animated-flower-image-0021.gif', done => {
-    request(app.processRequest.bind(app))
+    request(app)
       .get('/images/animated-flower-image-0021.gif')
       .expect('Content-Type', 'image/gif')
       .expect(200, done);
   });
 
   it('should give the index.html page and should pass the data to the server when the url is /', done => {
-    request(app.processRequest.bind(app))
+    request(app)
       .get('/')
       .send('name=flower')
-      .expect('Content-Type', 'text/html')
+      .expect('Content-Type', /text\/html/)
       .expect(200, done)
       .expect(/Flower Catalog/);
   });
 });
 
-describe('Not Allowed Method', () => {
-  it('should give 400 status code when the method is not allowed', done => {
-    request(app.processRequest.bind(app))
-      .put('/guestBook.html')
-      .expect('Content-Type', 'text/plain')
-      .expect('Method Not Allowed')
-      .expect(400, done);
-  });
-});
-
 describe('POST method', () => {
-  before(() => sinon.replace(fs, 'writeFileSync', () => {}));
-
   it('should be able to handle post request', done => {
-    request(app.processRequest.bind(app))
+    request(app)
       .post('/saveComments')
       .send('name=nooraNasrin&comment=hai')
       .expect('Location', 'guestBook.html')
       .expect(301, done);
   });
-
-  after(() => sinon.restore());
 });
